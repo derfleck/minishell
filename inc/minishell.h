3aa4f7276a01d4b6aec4104d87e09d49f5092851
rmocsai <rekamocsai@gmail.com>
2023-04-21 13:46:17 +0200

    struct for env copying

--- inc/minishell.h
+++ inc/minishell.h
@@ -8,6 +8,22 @@
 # include <sys/wait.h>
 # include <signal.h>
 
+/* global variable for exit status */
 extern int	g_stat;
+
+/* Struct for nodes */
+typedef struct s_env_node
+{
+	char				*key;
+	char				*value;
+	struct s_env_node	*next;
+}	t_env_node;
+
+/* Struct For Environment List */
+typedef struct s_env
+{
+	struct s_env_node	*head;
+	int					size;
+}	t_env;
 
 #endif
