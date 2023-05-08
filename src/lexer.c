bc4d3780f5c85e9f04a6bf296b43f363e055f39d
Michael Leitner <mleitner@c4r4p5.42vienna.com>
2023-05-08 17:13:26 +0200

    completed most of the lexer, still need to add them to minishell executable

--- src/lexer.c
+++ src/lexer.c
@@ -1,76 +1,20 @@
-//#include "../inc/minishell.h"
+#include "../inc/minishell.h"
 
-#include "../libft/libft.h"
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
+typedef enum s_type {
+	PIPE = 1,
+	GREAT,
+	GREAT_GREAT,
+	LESS,
+	LESS_LESS,
+}	t_type;
 
-//counts total number of commands in trimmed string
-static int	cnt_cmd(char *str, char *set)
-{
-	int		i;
-	int		cnt;
-	char	*trim;
-
-	i = 0;
-	cnt = 0;
-	trim = ft_strtrim(str, set);
-	if (ft_strchr(set, trim[i]) == NULL)
-		cnt++;
-	while (trim[i])
-	{
-		if (ft_strchr(set, trim[i]) != NULL)
-			cnt++;
-		i++;
-	}
-	free(trim);
-	return (cnt);
-}
-
-//checks size of command based on character set
-size_t	get_cmd_size(char *str, char *set)
-{
-	size_t	size;
-
-	size = 0;
-	while (*str && ft_strchr(set, *str) == 0)
-	{
-		size++;
-		str++;
-	}
-	return (size);
-}
-
-//splits string based if character in set is found
-char	**ft_split_set(char *str, char *set)
-{
-	char	**cmd;
-	int		i;
-	int		j;
-	int		len;
-
-	if (!str || !set)
-		return (NULL);
-	len = cnt_cmd(str, set);
-	cmd = ft_calloc(len + 1, sizeof(char *));
-	if (!cmd)
-		return (NULL);
-	i = 0;
-	while (i < len && *str)
-	{
-		while (*str && ft_strchr(set, *str) != 0)
-			str++;
-		cmd[i] = ft_calloc(get_cmd_size(str, set) + 1, sizeof(char));
-		if (cmd[i] == NULL)
-			return (NULL);
-		//TODO: add free if something fails
-		j = 0;
-		while (*str && ft_strchr(set, *str) == 0)
-			cmd[i][j++] = *str++;
-		i++;
-	}
-	return (cmd);
-}
+typedef struct s_lexer {
+	char			*str;
+	t_type			token;
+	int				i;
+	struct s_lexer	*prev;
+	struct s_lexer	*next;
+}	t_lexer;
 
 /*
 int	main(void)
@@ -78,14 +22,12 @@ int	main(void)
 	char	**str;
 	int	i = 0;
 
-	str = ft_split_set("ls -l | wc -l", " ");
+	str = ft_split_set("ls -l | wc -l >> test", " ");
 	while (str[i])
 		printf("%s\n", str[i++]);
 	return (i);
 }
 */
-
-//only syntax check after this line
 
 //skips quotes in string, returns value how many characters to skip
 int	skip_quotes(char *str)
@@ -107,6 +49,8 @@ int	skip_quotes(char *str)
 }
 
 //counts how many times c occurs in str, ignores quoted text
+//not really necessary for parantheses, as subject specifies
+//no special characters should be handled which aren't mentioned
 int	count_char(char *str, char c)
 {
 	int	i;
@@ -126,13 +70,14 @@ int	count_char(char *str, char c)
 }
 
 //checks if line entered has a correct syntax
-//first check: counts if # of brackets matches up
+//if an odd number of quotes is encountered, heredoc should be launched
+//with quote/dquote as prompt until missing character is used (maybe?)
 int	syntax_check(char *str)
 {
 	int	i;
 
 	i = 0;
-	if (count_char(str, '(') != count_char(str, ')'))
+	if ((count_char(str, '\'') % 2 == 1) || (count_char(str, '"') % 2 == 1))
 		return (0);
 	while (str[i])
 	{
@@ -143,4 +88,80 @@ int	syntax_check(char *str)
 			i++;
 	}
 	return (1);
+}
+
+//checks if character is a separating token (|/</>)
+t_type	check_token(char c)
+{
+	static int	token[3][2] = {{'|', PIPE}, \
+								{'>', GREAT}, \
+								{'<', LESS}};
+	int			i;
+
+	i = 0;
+	while (i < 3)
+	{
+		if (c == token[i][0])
+			return (token[i][1]);
+		i++;
+	}
+	return (0);
+}
+
+//classifies a one or two character string as token
+t_type	classify_token(char	*str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (i++ > 1)
+			return (0);
+	}
+	i = 0;
+	while (str[i])
+	{
+		if (str[i + 1])
+		{
+			if (check_token(str[i]) == GREAT && \
+			check_token(str[i + 1] == GREAT))
+				return (GREAT_GREAT);
+			else if (check_token(str[i]) == LESS && \
+			check_token(str[i + 1] == LESS))
+				return (LESS_LESS);
+		}
+		else
+			return (check_token(str[i]));
+		i++;
+	}
+	return (0);
+}
+
+//creates new linked list for lexer, needs token count
+t_lexer	*new_lexer_list(char **cmd, int i)
+{
+	int		j;
+	t_lexer	*lex;
+
+	j = 0;
+	lex = ft_calloc(sizeof(t_lexer), i);
+	if (!lex)
+		return (NULL);
+	while (cmd[j])
+	{
+		lex[j].i = j;
+		lex[j].str = cmd[i];
+		lex[j].token = classify_token(cmd[i]);
+		if (j < i - 1)
+			lex[j].next = lex[j + 1];
+		else
+			lex[j].next = lex[0];
+		if (j == 0)
+			lex[j].prev = lex[i - 1];
+		else
+			lex[j].prev = lex[j - 1];
+		j++;
+	}
+	return (lex);
 }
